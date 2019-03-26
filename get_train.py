4e6f4c8ea7177bcaaecad61d35a041b9ce0cdb8a
Catiks <OzhangsenO@gmail.com>
2019-03-26 10:15:34 +0800

    first

--- get_train.py
+++ get_train.py
@@ -4,6 +4,10 @@ import json
 
 if __name__ == '__main__':
     js = codecs.open('train_list', 'r', 'utf-8')
+    out = codecs.open('train_list.json','w', 'utf-8')
     data = js.read()
     jsondata = json.loads(data.strip('var train_list ='))
-    print(jsondata['2019-04-01']['T'])
+    out.write(json.dumps(jsondata, ensure_ascii=False))
+    js.close()
+    out.close()
+
